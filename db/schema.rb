b90788fb2399ad5edaadd76c305b6fec3a1c2e12
Thomas Ben <46992388+ThomBen@users.noreply.github.com>
2022-08-16 15:48:07 +0200

    task list with simple design

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_094318) do
-  create_table "restaurants", force: :cascade do |t|
-    t.string "name"
+ActiveRecord::Schema[7.0].define(version: 2022_08_16_100050) do
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
     t.text "details"
-    t.boolean "completed", default: false
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
